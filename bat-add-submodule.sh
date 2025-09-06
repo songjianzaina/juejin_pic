ed987a9aaa1743b5dc9a5ac9ad27362ea58aa2ce
songjian <songjianzaina@163.com>
2025-09-06 09:57:57 +0800

    add all

--- bat-add-submodule.sh
+++ bat-add-submodule.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
  
- 
+# 执行 ssh-add ~/.ssh/id_rsa  先将密钥添加到ssh-agent中  可以免去ssh密码输入
+
 # 循环添加子模块
-for i in {5..10}; do
+for i in {47..54}; do
 
   git submodule add -b master  "git@github.com:songjianzaina/juejin_p${i}.git"
 done
